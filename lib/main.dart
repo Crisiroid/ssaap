d1752bd0a72005015c602f721bba9d27bf2396aa
Crisiroid <csiroid@gmail.com>
2023-09-25 18:34:25 +0330

    Update: Added MVC Models Created Model and View Pages Created HomePage

---
+++ lib/main.dart
@@ -0,0 +1,18 @@
+import 'package:flutter/material.dart';
+import 'package:ssaap/View/Home.dart';
+
+void main() {
+  runApp(ssaap());
+}
+
+class ssaap extends StatelessWidget {
+  const ssaap({super.key});
+
+  @override
+  Widget build(BuildContext context) {
+    return MaterialApp(
+      title: "SSAAP - Your Ultimate Food Recipe Solution!",
+      home: HomePage(),
+    );
+  }
+}
