50d49fc0cd240747c3c7b3931d6f28e95098c181
Crisiroid <csiroid@gmail.com>
2023-09-25 18:56:39 +0330

    Update: Added Demo to Show Categories

--- lib/View/Home.dart
+++ lib/View/Home.dart
@@ -1,6 +1,7 @@
 // ignore_for_file: prefer_const_constructors
 
 import 'package:flutter/material.dart';
+import 'package:ssaap/Model/RecipeCategory.dart';
 
 class HomePage extends StatefulWidget {
   const HomePage({super.key});
@@ -10,12 +11,31 @@ class HomePage extends StatefulWidget {
 }
 
 class _HomePageState extends State<HomePage> {
+  late Future<List<Category>> Categories;
+
+  @override
+  void initState() {
+    // TODO: implement initState
+    super.initState();
+    Categories = AllCategories();
+  }
+
   @override
   Widget build(BuildContext context) {
     return Scaffold(
       appBar: AppBar(title: Text("SsAaP")),
       body: Center(
-        child: Text("SsAaP"),
+        child: FutureBuilder(
+          future: Categories,
+          builder: (context, snapshot) {
+            if (snapshot.hasData) {
+              return Text("data");
+            } else if (snapshot.hasError) {
+              return Text(snapshot.error.toString());
+            }
+            return CircularProgressIndicator();
+          },
+        ),
       ),
     );
   }
