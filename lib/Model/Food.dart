7e94e06dccde5f0bf20feb53fc01568f7993d2a5
Crisiroid <csiroid@gmail.com>
2023-09-26 10:26:25 +0330

    Update: Added Food Model

---
+++ lib/Model/Food.dart
@@ -0,0 +1,10 @@
+class Food {
+  final String strMeal;
+  final String strMealThumb;
+  final String idMeal;
+
+  const Food(
+      {required this.strMeal,
+      required this.strMealThumb,
+      required this.idMeal});
+}
